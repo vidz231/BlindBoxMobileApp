c37e82bf690e6c1792b68a848035fc6c7b1f051c
hoangminhle1108 <147319801+hoangminhle1108@users.noreply.github.com>
2025-06-08 19:57:30 +0700

    Update OrderDetailScreen.kt

--- feature/order/src/main/java/com/vidz/order/detail/OrderDetailScreen.kt
+++ feature/order/src/main/java/com/vidz/order/detail/OrderDetailScreen.kt
@@ -10,10 +10,9 @@ import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
+import com.vidz.order.model.OrderProductItem
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
